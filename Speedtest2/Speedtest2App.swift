1deb3b13ddd89ea5947f6a7d1a67947ab5e0a492
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2020-12-31 17:10:41 +0300

    Initial Commit

---
+++ Speedtest2/Speedtest2App.swift
@@ -0,0 +1,17 @@
+//
+//  Speedtest2App.swift
+//  Speedtest2
+//
+//  Created by Batool  Jawaheri  on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct Speedtest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
