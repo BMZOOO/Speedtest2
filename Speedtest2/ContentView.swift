ae997293e727c04c927569c6027a3b1d39c8a964
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2020-12-31 17:12:13 +0300

    speedtest

--- Speedtest2/ContentView.swift
+++ Speedtest2/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Speedtest2
+//  SPEEDTEST
 //
 //  Created by Batool  Jawaheri  on 12/31/20.
 //
@@ -9,8 +9,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+      
+          
+        ZStack{
+            Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+                .ignoresSafeArea()
+            VStack{
+                
+            HStack{
+                Text("top left")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .clipShape(Capsule())
+                    Spacer()
+                
+                Text("top right")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.yellow)
+                    .clipShape(Capsule())
+            }
             .padding()
+            Spacer()
+                
+                HStack{
+                    Text("Mid left")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        Spacer()
+                    
+                    Text("Mid center ")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.red)
+                        .clipShape(Capsule())
+                    Spacer()
+                    
+                    Text("Mid right")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.yellow)
+                        .clipShape(Capsule())
+                }
+                .padding()
+                Spacer()
+                
+                HStack{
+                    Text("bottom left")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        Spacer()
+                    
+                    
+                    
+                    Text("bottom right")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.yellow)
+                        .clipShape(Capsule())
+                }
+                .padding()
+                
+                
+                
+        }
+        }
+           
+        
     }
 }
 
